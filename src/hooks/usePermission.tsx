51d20aa919d5d052bb6ecb9cf0f8fe0b8aae5d72
felixelement <felix@element6.io>
2023-10-29 19:55:52 -0400

    ios only

--- src/hooks/usePermission.tsx
+++ src/hooks/usePermission.tsx
@@ -3,6 +3,8 @@ import AppleHealthKit, {
   HealthKitPermissions,
 } from 'react-native-health';
 
+import { Platform } from 'react-native';
+
 const permissions: HealthKitPermissions = {
   permissions: {
     read: [
@@ -24,6 +26,27 @@ const usePermission = (): IPermission => {
     useState(false);
 
   useEffect(() => {
+    if (Platform.OS !== 'ios') {
+      return;
+    }
+
+    AppleHealthKit.isAvailable(
+      (err: Object, available: boolean) => {
+        if (err) {
+          console.log(
+            'error initializing Healthkit: ',
+            err
+          );
+          return;
+        }
+        if (!available) {
+          console.log(
+            'Apple HealthKit is not available'
+          );
+          return;
+        }
+      }
+    );
     AppleHealthKit.initHealthKit(
       permissions,
       (err: string) => {
