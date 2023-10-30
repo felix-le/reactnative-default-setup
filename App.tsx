a4419e891d58f12702e402b6294423c1b8525414
felixelement <felix@element6.io>
2023-10-29 22:08:49 -0400

    setup default

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
 import { View, Text } from 'react-native';
-
+import FitnessIcon from './src/components/FitnessIcon';
 interface AppProps {}
 
 export default function App(props: AppProps) {
   return (
     <View className='flex-1 bg-neutral-700 justify-center p-5'>
+      <View className='flex justify-center items-center mb-10'>
+        <FitnessIcon
+          width={60}
+          height={60}
+          color='#EBC401'
+        />
+      </View>
       <Text className='text-white text-center'>
         hello from default
       </Text>
