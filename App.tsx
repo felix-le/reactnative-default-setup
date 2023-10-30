13cdbc30373aba84ea4b079a64ccba196690048e
felixelement <felix@element6.io>
2023-10-29 22:06:16 -0400

    setup default

--- App.tsx
+++ App.tsx
@@ -1,39 +1,13 @@
-import { View } from 'react-native';
-
-import Value from './src/components/Value';
-import FitnessIcon from './src/components/FitnessIcon';
-
-import useHealthData from './src/hooks/useHealthData';
+import { View, Text } from 'react-native';
 
 interface AppProps {}
 
 export default function App(props: AppProps) {
-  const { steps, distance } = useHealthData(
-    new Date(2023, 9, 25)
-  );
-
   return (
     <View className='flex-1 bg-neutral-700 justify-center p-5'>
-      <View className='flex justify-center items-center mb-10'>
-        <FitnessIcon
-          width={60}
-          height={60}
-          color='#EBC401'
-        />
-      </View>
-
-      <View className='flex flex-row flex-wrap'>
-        <Value
-          label='Steps'
-          value={steps.toString()}
-        />
-        <Value
-          label='Distance'
-          value={`${(distance / 1000).toFixed(
-            2
-          )} km`}
-        />
-      </View>
+      <Text className='text-white text-center'>
+        hello from default
+      </Text>
     </View>
   );
 }
--- src/hooks/usePermission.tsx
+++ src/hooks/usePermission.tsx
@@ -1,21 +1,4 @@
-import { useEffect, useState } from 'react';
-import AppleHealthKit, {
-  HealthKitPermissions,
-} from 'react-native-health';
-
-import { Platform } from 'react-native';
-
-const permissions: HealthKitPermissions = {
-  permissions: {
-    read: [
-      AppleHealthKit.Constants.Permissions
-        .StepCount,
-      AppleHealthKit.Constants.Permissions
-        .DistanceWalkingRunning,
-    ],
-    write: [],
-  },
-};
+import { useState } from 'react';
 
 interface IPermission {
   hasPermissions: boolean;
@@ -24,43 +7,6 @@ interface IPermission {
 const usePermission = (): IPermission => {
   const [hasPermissions, setHasPermissions] =
     useState(false);
-
-  useEffect(() => {
-    if (Platform.OS !== 'ios') {
-      return;
-    }
-
-    AppleHealthKit.isAvailable(
-      (err: Object, available: boolean) => {
-        if (err) {
-          console.log(
-            'error initializing Healthkit: ',
-            err
-          );
-          return;
-        }
-        if (!available) {
-          console.log(
-            'Apple HealthKit is not available'
-          );
-          return;
-        }
-      }
-    );
-    AppleHealthKit.initHealthKit(
-      permissions,
-      (err: string) => {
-        if (err) {
-          console.log(
-            'error initializing Healthkit: ',
-            err
-          );
-          return;
-        }
-        setHasPermissions(true);
-      }
-    );
-  }, []);
 
   return { hasPermissions };
 };
